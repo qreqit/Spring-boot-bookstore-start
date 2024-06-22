820718ead5e128e7c203ce45e0463f7276f39d90
Ivan Prystaia <greqit@gmail.com>
2024-06-22 12:07:46 +0300

    main

---
+++ src/main/java/com/greqit/start/SprintBookstoreStrartApplication.java
@@ -0,0 +1,13 @@
+package com.greqit.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBookstoreStrartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBookstoreStrartApplication.class, args);
+    }
+
+}
